103a60555e9231f8a3d2d26fededf39d6846fe97
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-09-08 12:34:22 +0200

    comment code and functions using jsDOC

--- script.js
+++ script.js
@@ -11,12 +11,28 @@ const textarea = querySelector("textarea"), fileNameInput = querySelector(".file
 //*
 //* Event Listeners
 //*
+
+/**
+ * getting selected option text
+ */
+selectMenu.addEventListener("change", () => {
+    let selectedOption = selectMenu.options[selectMenu.selectedIndex].text;
+    saveBtn.innerText = `Save as ${selectedOption.split(" ")[0]}`
+})
+
+/**
+ * creating a Blob and reference context to link and finally download it to Host OS
+ *      ? URL.createObjectURL creates a url that represent passed object
+ *      ? creating <a> Tag
+ *      ? passing filename as download value of link
+ *      ? passing fileUrl as href value of link
+ *      ? execute the link
+ */
 saveBtn.addEventListener("click", () => {
     const blob = new Blob([textarea.value], {type: selectMenu.value})
-    //? URL.createObjectURL creates a url that represent passed object
     const fileUrl = URL.createObjectURL(blob)
-    const link = document.createElement("a"); //? creating <a> Tag
-    link.download = fileNameInput.value; //? passing filename as download value of link
-    link.href = fileUrl; //? passing fileUrl as href value of link
-    link.click() //? execute the link
+    const link = document.createElement("a");
+    link.download = fileNameInput.value;
+    link.href = fileUrl;
+    link.click()
 })
