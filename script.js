cd747b1a693468e0747354739ecb63064bb0b47f
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-09-08 12:24:44 +0200

    clean up code

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+//*
+//* Global Functions
+//*
 const querySelector = (target => document.querySelector(target));
 
-const textarea = querySelector("textarea"),
-    fileNameInput = querySelector(".file-name input"),
-    selectMenu = querySelector(".save-as select"),
-    saveBtn = querySelector(".save-btn");
-
+//*
+//* Variables
+//*
+const textarea = querySelector("textarea"), fileNameInput = querySelector(".file-name input"),
+    selectMenu = querySelector(".save-as select"), saveBtn = querySelector(".save-btn");
+//*
+//* Event Listeners
+//*
 saveBtn.addEventListener("click", () => {
     const blob = new Blob([textarea.value], {type: selectMenu.value})
-    console.log(blob)
+    //? URL.createObjectURL creates a url that represent passed object
+    const fileUrl = URL.createObjectURL(blob)
+    const link = document.createElement("a"); //? creating <a> Tag
+    link.download = fileNameInput.value; //? passing filename as download value of link
+    link.href = fileUrl; //? passing fileUrl as href value of link
+    link.click() //? execute the link
 })
