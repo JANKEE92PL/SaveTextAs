2ed72c7e62bf46adc026781bc153860c1a03b932
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-09-08 14:21:33 +0200

    Create Test.js

---
+++ test.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict';
+
+    /**
+     * test function
+     * @param {string} desc
+     * @param {function} fn
+     */
+    function it(desc, fn) {
+        try {
+            fn();
+            console.log('\x1b[32m%s\x1b[0m', '\u2714 ' + desc);
+        } catch (error) {
+            console.log('\n');
+            console.log('\x1b[31m%s\x1b[0m', '\u2718 ' + desc);
+            console.error(error);
+        }
+    }
+
+    function assert(isTrue) {
+        if (!isTrue) {
+            throw new Error();
+        }
+    }
+
+//! EXAMPLES
+    /**
+     *? Expample to FAILED The test
+     */
+    // it('should fail', function () {
+    //     assert(1 !== 1);
+    // });
+    /**
+     *? Expample to PASSING The test
+     */
+    // it('should pass', function () {
+    //     assert(1 === 1);
+    // });
+    // ! EXAMPLES END
+
+})();
